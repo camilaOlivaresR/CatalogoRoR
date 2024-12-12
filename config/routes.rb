8ac6f8dd7b22a7d31add840175c5ad0079b1d2df
cami oliva <lamica.olivares@gmail.com>
2024-12-12 13:43:31 -0300

    proyecto catalogo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users
   resources :products
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -8,5 +10,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-   root "products#index"
+   root "home#index"
 end
